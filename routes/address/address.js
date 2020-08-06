0916404cf2c0fce5eb29d616899ccefb795eabd1
ibrahimafatima <sakho92iba@gmail.com>
2020-08-06 16:38:39 +0100

    commit 6

--- routes/address/address.js
+++ routes/address/address.js
@@ -4,12 +4,10 @@ const isAuth = require("../../middleware/isAuth");
 
 const router = express.Router();
 
-router.post("/", isAuth, async (req, res) => {
+router.post("/", async (req, res) => {
   const new_address = new Address({
     address: req.body.address,
     contact: req.body.contact,
-    pharmacy_name: req.userToken.pharmacy_name,
-    secret_key: req.userToken.secret_key,
   });
 
   const result = await new_address.save();
